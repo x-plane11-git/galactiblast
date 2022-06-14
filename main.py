a8e424a796765db1ae22bc3f510cc82749a9c824
x-plane11-git <81062925+x-plane11-git@users.noreply.github.com>
2022-06-13 22:44:26 -0400

    Rework

--- main.py
+++ main.py
@@ -1,82 +1,29 @@
-#sideShooter.py
-from pygame import *
-from random import *
+import pygame
+import random
+import time 
 
-width,height=800,600
-screen=display.set_mode((width,height))
-RED=(255,0,0)
-GREY=(127,127,127)
-BLACK=(0,0,0)
-BLUE=(0,0,255)
-GREEN=(0,255,0)
-YELLOW=(255,255,0)
-rapid=20
+width,height = 1280,780
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Invaders Based Game')
+#loading all the images
+#Player Ship
+playerShip = pygame.image.load('assets/graphics/PNG/Spaceships/05/Spaceship_05_BLUE.png')
+#Enemy Ship
+EnemyShip1 = pygame.image.load('assets/graphics/PNG/Spaceships/01/Spaceship_01_RED.png')
+EnemyShip2 = pygame.image.load('assets/graphics/PNG/Spaceships/01/Spaceship_01_RED.png')
+EnemyShip3 = pygame.image.load('assets/graphics/PNG/Spaceships/01/Spaceship_01_RED.png')
+EnemyShip4 = pygame.image.load('assets/graphics/PNG/Spaceships/001/Spaceship_01_RED.png')
+#Lasers
+redLaser = pygame.image.load('assets/graphics/PNG/redLaser.png')
+blueLaser = pygame.image.load('assets/graphics/PNG/blueLaser.png')
 
-def drawScene(screen,g,bull,targ):
-    screen.fill((255,255,0))
-    
-    for b in bull:
-        brect=Rect(b[0],b[1],10,10)#bullets size is 10x10
-        draw.rect(screen,(0,0,255),brect)
-    for t in targ:
-        draw.rect(screen,(0,0,255),t)
-    draw.circle(screen,(255,0,0),(50,g[1]),20)
-    display.flip()
+#Background
+pygame.screen.fill(0)
 
-
-def moveBullets(bull):
-    for b in bull:
-        b[0]+=b[2]#horizontal movement
-        b[1]+=b[3]#vertical movement
-        if b[0]>800:#off-screen
-            bull.remove(b)
-
-def checkHits(bull,targ):
-    for b in bull:#go through each bullet
-        for t in targ:
-            brect=Rect(b[0],b[1],10,10)
-            if brect.colliderect(t):
-                targ.remove(t)
-                bull.remove(b)
-                break
-    
-v=[5,0] #5 is the horizontal speed, 0 is vertical speed    
-
-guy=[50,300]
-targets=[]
-##bullets=[[170,45,5,1],[400,528,1,-4]]
-bullets=[]
-#creating enemies at random locations
-for i in range(5):
-    targets.append(Rect(randint(400,750),randint(100,500),40,40))
-
-#print(targets)
-
-running=True
-myClock=time.Clock()
-while running:
-    for evt in event.get():
-        if evt.type==QUIT:
-            running=False
-                       
-    mx,my=mouse.get_pos()
-    mb=mouse.get_pressed()
-    keys=key.get_pressed()
-
-
-    guy[1]=my
-
-    if rapid<20:
-        rapid+=1
-    print(bullets)
-    
-    drawScene(screen,guy,bullets,targets)
-    moveBullets(bullets)
-    checkHits(bullets,targets)
-
-    if keys[32] and rapid==20:
-        bullets.append([70,guy[1],v[0],v[1]])
-        rapid=0
-
-    myClock.tick(60)       
-quit()
+#Functions
+def main():
+    run = True
+    FPS = 60 #for pygame clock
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(FPS)
