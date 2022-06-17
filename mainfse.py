d4f8ee00246647a6e73b20b2a05a896fbc3f55b0
x-plane11-git <81062925+x-plane11-git@users.noreply.github.com>
2022-06-17 05:52:15 -0400

    v. 0.6

--- mainfse.py
+++ mainfse.py
@@ -45,9 +45,14 @@ buttonRect2 = Rect(480, 445, 320, 70)
 buttonRect3 = Rect(480, 525, 320, 70)
 buttonRect4 = Rect(480, 605, 320, 70)
 borderRect = Rect(0, 0, 1280, 720)
+levelRect1=Rect(210,165,550,180)
+levelRect2=Rect(550,445,550,180)
+levelRect11=Rect(210,165,550,180)
+levelRect22=Rect(550,445,550,180)
 # FONT LOADING
 font = pygame.font.SysFont("tahoma", 40)
 font1 = pygame.font.SysFont("tahoma", 20)
+font3=pygame.font.SysFont("tahoma",150)
 # GAME IMAGE LOADING
 # Player Ship
 playerVehicle = image.load(
@@ -74,8 +79,10 @@ bullxChange = 0
 bullyChange = 0.7
 bullState = 'ready'
 # Background
-bg = image.load('assets/graphics/PNG/Space Background.png').convert_alpha()
+level1bg = image.load('assets/graphics/PNG/Space Background.png').convert_alpha()
+lev2bg = image.load('assets/graphics/PNG/bg2.jpg').convert_alpha()
 storybg = image.load('pics/storyPic.jpg').convert_alpha()
+levelbg = image.load("pics/bg2.jpg").convert_alpha()
 #Extra Variables (local):
 score = 0
 def sound():
@@ -84,8 +91,32 @@ def sound():
 
 def settings():
     pass
+def levels():
+    run = True
+    screen.blit(levelbg,(0,0))
+    while run:
+        for evt in event.get():
+            if evt.type==QUIT:
+                sys.exit()
+                        
+        mx,my=mouse.get_pos()
+        mb=mouse.get_pressed()
+        
+        draw.rect(screen,BLACK,levelRect1,0,10)
+        draw.rect(screen,BLACK,levelRect2,0,10)
+        draw.rect(screen,WHITE,levelRect11,10,10)
+        draw.rect(screen,WHITE,levelRect22,10,10)
+
+        screen.blit(font3.render("Level 1",True,WHITE),(250,170))
+        screen.blit(font3.render("Level 2",True,WHITE),(590,445))
 
 
+        if mb[0]:
+            if levelRect1.collidepoint(mx,my):
+                return 'lev1'
+            if levelRect2.collidepoint(mx,my):
+                return 'lev2'
+        display.flip()
 def instructions():
     run = True
     instructions = image.load('pics/instructions.jpg').convert_alpha()
@@ -153,7 +184,7 @@ def collided(enemyX, enemyY, bulletX, bulletY):
         return False
 
 
-def mainGame():
+def lev1():
     # sprite rect assign
     fpsClock = pygame.time.Clock()
     targets = []
@@ -174,11 +205,10 @@ def mainGame():
     yMove = 0
     run = True
     speed = 0.5
-    screen.blit(bg, (0, 0))
     if cooldown < 20:
         cooldown += 1
     while run:
-        screen.blit(bg, (0, 0))
+        screen.blit(level1bg, (0, 0))
         keys = key.get_pressed()
         mx, my = mouse.get_pos()
         mb = mouse.get_pressed()
@@ -224,6 +254,76 @@ def mainGame():
         display.flip()
     fpsClock.tick(60)
 
+def lev2():
+    # sprite rect assign
+    fpsClock = pygame.time.Clock()
+    targets = []
+    bullets = []
+    cooldown = 20
+    global px
+    global py
+    global bullx
+    global bully
+    global bullState
+    global enemyXchange
+    global enemyX
+    global enemyY
+    global enemyYchange
+    global score
+    enemyXchange = 0.5
+    psize = 40
+    xMove = 0
+    yMove = 0
+    run = True
+    speed = 0.5
+    if cooldown < 20:
+        cooldown += 1
+    while run:
+        screen.blit(lev2bg, (0, 0))
+        keys = key.get_pressed()
+        mx, my = mouse.get_pos()
+        mb = mouse.get_pressed()
+        for evt in event.get():
+            if evt.type == QUIT:
+                return 'menu'
+        keys = key.get_pressed()  # check what keys are pressed
+        if keys[K_LEFT]:  # and px - speed > 0: #moving player left
+            px -= speed
+        if keys[K_RIGHT]:  # and px + speed: #moving player right
+            px += speed
+        if keys[K_SPACE]:
+            bullx = px
+            fireBullet(bullx, bully)
+        player = draw.rect(screen, RED, [px, py, psize, psize])
+        enemy(enemyX, enemyY)
+        # bullets
+        if bully <= 0:
+            bully = 595
+            bullState = 'ready'
+        if bullState == 'fire':
+            fireBullet(bullx, bully)
+            bully -= bullyChange
+        # enemy movement
+        enemyX += enemyXchange
+        if enemyX <= 0:
+            enemyXchange = 0.5
+            enemyY += enemyYchange
+        elif enemyX >= 1100:
+            enemyXchange = -0.5
+            enemyY += enemyYchange
+        #collision
+        collision = collided(enemyX,enemyY,bullx,bully)
+        if collision:
+            bully = 595
+            bullState = 'ready'
+            score += 1
+            enemyX = random.randint(5, 1100)
+            enemyY = random.randint(50, 100)
+        px = px + xMove
+        py = py + yMove
+        screen.blit(font1.render("Score:"+str(score), False, WHITE), (1000, 685))
+        display.flip()
+    fpsClock.tick(60)
 
 def start():
     run = True
@@ -259,7 +359,7 @@ def start():
 
         if mb[0]:
             if startRect.collidepoint(mx, my):
-                return "lev1"
+                return "levels"
             if charRect.collidepoint(mx, my):
                 button = "Character Select"
             if rulesRect.collidepoint(mx, my):
@@ -278,8 +378,12 @@ page = "menu"
 while page != "exit":
     if page == "menu":
         page = start()
-    if page == "lev1":
-        page = mainGame()
+    if page == "levels":
+        page = levels()
+    if page == 'lev1':
+        page = lev1()
+    if page == 'lev2':
+        page = lev2()
     if page == 'story':
         page = story()
     if page == 'instructions':
